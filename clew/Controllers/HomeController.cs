465505ea2411631752b12456c8df88e4d270ebc0
Eugen Podaru <eugen.podaru@live.com>
2016-12-10 10:10:14 +0100

    add assets and index controller

---
+++ clew/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace clew.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
