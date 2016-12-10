d13a2b17faf2602b1685339b1e31c87e0e0fdb89
Eugen Podaru <eugen.podaru@live.com>
2016-12-10 17:52:37 +0100

    add azure vision client

--- azure/Properties/AssemblyInfo.cs
+++ azure/Properties/AssemblyInfo.cs
@@ -5,14 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("rdw.tests")]
+[assembly: AssemblyTitle("azure")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("rdw.tests")]
+[assembly: AssemblyProduct("azure")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a982f285-5899-4bae-8b55-7d396b34b24c")]
 
 // Version information for an assembly consists of the following four values:
 //
